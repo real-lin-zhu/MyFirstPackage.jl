1194bd916d815a0527adbb341687e9687bcd6d2d
lin_zhu <lzhu957@connect.hkust-gz.edu.cn>
2025-09-18 20:55:37 +0800

    Initial commit

--- src/MyFirstPackage.jl
+++ src/MyFirstPackage.jl
@@ -1,5 +1,13 @@
 module MyFirstPackage
 
+using LinearAlgebra
+
+# Export public interfaces
+export Lorenz, integrate_step
+export Point, Point2D, Point3D
+export RungeKutta, Euclidean
+
+include("lorenz.jl")
 # Write your package code here.
 
 end
